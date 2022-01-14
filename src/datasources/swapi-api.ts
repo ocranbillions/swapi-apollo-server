5275e8c6522cf7caa3916a83eb5bd5519974a3ab
Samuel Ocran <sammiestt@gmail.com>
2022-01-14 10:52:33 +0100

    Add return types to api responses

--- src/datasources/swapi-api.ts
+++ src/datasources/swapi-api.ts
@@ -1,19 +1,39 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
 
+interface Homeworld {
+  name: string
+  rotation_period: string
+  orbital_period: string
+  diameter: string
+  climate: string
+  gravity: string
+  terrain: string
+  surface_water: string
+  population: string
+}
+
+interface Person {
+  name: string
+  height: string
+  mass: string
+  gender: string
+  homeworld: Homeworld
+}
+
 class SwapiAPI extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = 'https://swapi.dev/api/';
   }
 
-  async fetchAllPeople(searchTerm: string) {
+  async fetchAllPeople(searchTerm: string): Promise<{data: Person[]}> {
     const people = await this.get(`people/?search=${searchTerm}`);
     return {
       data: people.results
     };
   }
 
-  async fetchHomeworld(homeworldUrl: string) {
+  async fetchHomeworld(homeworldUrl: string): Promise<Homeworld> {
     const endpoint = homeworldUrl.substring(22)
     return await this.get(endpoint);
   }
